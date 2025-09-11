f0a273b28c27441db6ee8a30c2146eda7927936d
lakmapuryogesh <lakmapuryogesh@gmail.com>
2025-09-11 20:30:35 +0530

    Initial commit - frontend + backend

--- src/components/weather-forcast.tsx
+++ src/components/weather-forcast.tsx
@@ -1,6 +1,7 @@
 import { format } from "date-fns";
 import type { ForecastData } from "./api/types"
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { ArrowDown, ArrowUp, Droplets, Wind } from "lucide-react";
 interface WeatherForecastProps{
     data:ForecastData;
 }
@@ -40,6 +41,8 @@ const WeatherForecast = ({data}:WeatherForecastProps) => {
    
    const nextDays = Object.values(dailyForcasts).slice(0,6);
 
+   const formatTemp =(temp: number)=>`${Math.round(temp)}°`;
+
     return (
     <Card>
   <CardHeader>
@@ -58,8 +61,27 @@ const WeatherForecast = ({data}:WeatherForecastProps) => {
             className="text-sm text-muted-foreground capitalize"
             >{day.weather.description}</p>
           </div>
-          <div></div>
-          <div></div>
+
+          <div className="flex justify-center gap-4">
+           <span className="flex items-center text-blue-500">
+               <ArrowDown className="mr-1 h-4 w-4"/>
+               {formatTemp(day.temp_min)}
+           </span>
+           <span className="flex items-center text-red-500">
+         <ArrowUp className="mr-1 h-4 w-4" />
+               {formatTemp(day.temp_max)}
+           </span>
+          </div>
+          <div className="flex justify-end gap-4">
+            <span className="flex items-center gap-1">
+              <Droplets className="h-4 w-4 text-blue-500" />
+              <span className="text-sm">{day.humidity}</span>
+            </span>
+            <span className="flex items-center gap-1">
+            <Wind className="h-4 w-4 text-blue-500"/>
+            <span className="text-sm">{day.wind}m/s</span>
+            </span>
+          </div>
         </div>
      })}
    </div>
